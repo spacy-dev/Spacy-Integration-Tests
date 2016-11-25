126bb8fa0d15c280bdf8ce97ee75f04dc1f190c7
Lars Lubkoll <lars.lubkoll@posteo.de>
2016-11-25 17:03:11 +0100

    [TRAVIS] Fix shared path for docker image

--- run_test_in_docker.sh
+++ run_test_in_docker.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 FENICS_HOME=/home/fenics
-FENICS_SHARED="${FENICS_HOME}/shared"
+DEPS="${FENICS_HOME}/shared/deps"
 
 # Install cmake
-cd $FENICS_SHARED
+cd $DEPS
 CMAKE_URL="http://www.cmake.org/files/v3.5/cmake-3.5.2-Linux-x86_64.tar.gz"
 mkdir cmake && wget --no-check-certificate --quiet -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C cmake
-export PATH=${FENICS_SHARED}/cmake/bin:${PATH}
+export PATH=${DEPS}/cmake/bin:${PATH}
 
 # Install gtest
 git clone https://github.com/google/googletest.git
@@ -16,7 +16,7 @@ sudo cp -r ../googletest/include/gtest /usr/local/include/
 sudo cp googlemock/gtest/lib*.a /usr/local/lib
 
 # Install Spacy
-cd ${FENICS_SHARED}
+cd ${DEPS}
 git clone https://github.com/spacy-dev/Spacy.git
 cd Spacy && mkdir build && cd build && cmake .. && make && sudo make install
 
