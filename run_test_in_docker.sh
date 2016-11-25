53b7c0d9c9f11883973b6eaf2af1203375976350
Lars Lubkoll <lars.lubkoll@posteo.de>
2016-11-25 22:21:19 +0100

    Fix script

--- run_test_in_docker.sh
+++ run_test_in_docker.sh
@@ -3,23 +3,27 @@
 FENICS_HOME=/home/fenics
 FENICS_SHARED="${FENICS_HOME}/shared"
 DEPS="${FENICS_SHARED}/deps"
+
+# Install cmake
+cd $DEPS
+CMAKE_URL="http://www.cmake.org/files/v3.5/cmake-3.5.2-Linux-x86_64.tar.gz"
+mkdir cmake && wget --no-check-certificate --quiet -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C cmake
 export PATH=${DEPS}/cmake/bin:${PATH}
 
 # Install gtest
-cd googletest/build
+git clone https://github.com/google/googletest.git
+cd googletest && mkdir build && cd build && cmake .. && make -j2
 sudo cp -r ../googletest/include/gtest /usr/local/include/
 sudo cp googlemock/gtest/lib*.a /usr/local/lib
 
 # Install Spacy
 cd ${DEPS}
-cd Spacy/build
-sudo make install
-#git clone https://github.com/spacy-dev/Spacy.git
-#cd Spacy && mkdir build && cd build && cmake .. -DDolfin=ON && make -j2 && sudo make install
+git clone https://github.com/spacy-dev/Spacy.git
+cd Spacy && mkdir build && cd build && cmake .. -DDolfin=ON && make -j2 && sudo make install
 
 # Run tests
 cd ${FENICS_SHARED}
 mkdir build && cd build
 cmake .. -DEigen=ON -DDolfin=ON
-make -j2 && ./FEniCS_vectorCreator
+make -j2 && ctest
 
